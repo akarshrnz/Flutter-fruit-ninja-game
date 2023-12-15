49f0a976bc6dc6d571a394ca8603f18b19522c62
akarshrnz <rnzakarsh15@gmail.com>
2023-12-15 11:14:10 +0530

    first commit

---
+++ android/app/src/main/kotlin/com/akarshrnz/fruit/ninja/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.akarshrnz.fruit.ninja
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flame/flame.dart';
+import 'package:flutter/material.dart';
+import 'package:flame/game.dart' hide Game;
+import 'package:flutter_fruit_ninja_game/src/game.dart';
+
+
+void main() {
+
+  WidgetsFlutterBinding.ensureInitialized();
+
+  Flame.device.fullScreen();
+  Flame.device.setLandscape();
+
+  runApp(GameWidget(game: MainRouterGame()));
+}
+
